1406f1f4d724f61f5869ce2e4076758e3985dcf9
Aliaksandr Baltruk <aliaksandr_baltruk@epam.com>
2015-03-16 18:46:29 +0300

    SharedPrefs added. All as strings. Save timeperiod.

--- app/src/main/java/com/by/alex/depositcalc/MainActivity.java
+++ app/src/main/java/com/by/alex/depositcalc/MainActivity.java
@@ -1,19 +1,50 @@
 package com.by.alex.depositcalc;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity implements OnClickListener {
+
+    Button btnCalc, btnAdd, btnCancel;
+    EditText edtCurrencyA, edtExcRateNow, edtSummAvalue, edtPercentA, edtBeginDate, edtTimeperiod, edtDateEnd;
+    SharedPreferences DefPref;
+
+    public static final String APP_PREFERENCES = "calcsettings";
+    public static final String CURRENCY_A = "BLR";
+    public static final String BEGIN_DATE = "01.01.2015";
+    public static final String END_DATE = "07.07.2017";
+    public static final String EXC_RATE_NOW = "15 000";
+    public static final String SUMM_A_VALUE = "1000000";
+    public static final String PERCENT_A = "56";
+    public static final String TIMEPERIOD = "30";
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        edtCurrencyA = (EditText) findViewById(R.id.edtCurrencyA);
+        edtExcRateNow = (EditText) findViewById(R.id.edtExcRateNow);
+        edtSummAvalue = (EditText) findViewById(R.id.edtSummAvalue);
+        edtPercentA = (EditText) findViewById(R.id.edtPercentA);
+        edtBeginDate = (EditText) findViewById(R.id.edtBeginDate);
+        edtTimeperiod = (EditText) findViewById(R.id.edtTimeperiod);
+        edtDateEnd = (EditText) findViewById(R.id.edtEndDate);
+
+        DefPref = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+        setDefSettings();
     }
 
 
@@ -38,9 +69,37 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 
         return super.onOptionsItemSelected(item);
     }
+    @Override
+    protected void onPause(){
+        SharedPreferences.Editor ed = DefPref.edit();
+        ed.putString(TIMEPERIOD, edtTimeperiod.getText().toString());
+        ed.commit();
+
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy(){
+        SharedPreferences.Editor ed = DefPref.edit();
+        ed.putString(TIMEPERIOD, edtTimeperiod.getText().toString());
+        ed.commit();
+
+        super.onDestroy();
+    }
 
     @Override
     public void onClick(View view) {
+
+    }
+
+    void setDefSettings(){
+         edtCurrencyA.setText(DefPref.getString(CURRENCY_A, "Bel rubl"));
+         edtBeginDate.setText(DefPref.getString(BEGIN_DATE, "01.01.2015"));
+         edtDateEnd.setText(DefPref.getString(END_DATE, "01.02.2015"));
+         edtExcRateNow.setText(DefPref.getString(EXC_RATE_NOW, "15000"));
+         edtSummAvalue.setText(DefPref.getString(SUMM_A_VALUE, "1000000"));
+         edtPercentA.setText(DefPref.getString(PERCENT_A, "50"));
+         edtTimeperiod.setText(DefPref.getString(TIMEPERIOD, "30"));
 
     }
 }
